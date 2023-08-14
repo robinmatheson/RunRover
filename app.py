c268d1634f726a92ce8b8479912bc06772a9de7c
Robin Matheson <robinm0211@gmail.com>
2023-08-14 13:33:25 -0700

    added example table and updated page layout

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template, request
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
 app = Flask(__name__)
 
 @app.route('/')
 
 def index():
-    return render_template('index.html')
+    return render_template('index.html',
+                            # runs = RUNS
+                            )
 
 if __name__ == "__main__":
     app.run(debug=True)
