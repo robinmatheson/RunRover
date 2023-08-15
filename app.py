4f37ecfc39d305294a4b41ad5858073f691c95b4
Robin Matheson <robinm0211@gmail.com>
2023-08-15 16:07:50 -0700

    prints elements in mysql db to screen. successful!

--- app.py
+++ app.py
@@ -14,44 +14,58 @@ db = SQLAlchemy()
 app = Flask(__name__)
 
 app.config.from_object(Config)
-# app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
 
 db.init_app(app)
 
 # with app.app_context():
 #     db.reflect()
 
-# from Run import Run
-# from routes import index
+class Run(db.Model):
+    __tablename__ = 'runs'
+    id = db.Column(db.Integer, primary_key=True)
+    type = db.Column(db.String, nullable = False)
+    date = db.Column(db.String(10), nullable = False)
+    time = db.Column(db.String(8), nullable = False)
+    distance = db.Column(db.Float, nullable = False)
+    pace = db.Column(db.String(8), nullable = False)
+    difficulty = db.Column(db.Integer, nullable = False)
 
-# class Run(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     run_type = db.Column(db.String, nullable = False)
-#     date = db.Column(db.String(10), nullable = False)
-#     time = db.Column(db.String(8), nullable = False)
-#     distance = db.Column(db.Float, nullable = False)
-#     pace = db.Column(db.String(8), nullable = False)
-#     difficulty = db.Column(db.Integer, nullable = False)
+    def __repr__(self):
+        return 'f<{self.run_type}, {self.date}>'
 
-#     def __repr__(self):
-#         return 'f<{self.run_type}, {self.date}>'
+#routes
 
 @app.route('/')
-
 # to test the database connection
-def testdb():
+# def testdb():
+#     try:
+#         db.session.query(text('1')).from_statement(text('SELECT 1')).all()
+#         return '<h1>It works.</h1>'
+#     except Exception as e:
+#         # e holds description of the error
+#         error_text = "<p>The error:<br>" + str(e) + "</p>"
+#         hed = '<h1>Something is broken.</h1>'
+#         return hed + error_text
+        
+def index():
     try:
-        db.session.query(text('1')).from_statement(text('SELECT 1')).all()
-        return '<h1>It works.</h1>'
+        runs = db.session.execute(db.select(Run)
+            .order_by(Run.date)).scalars()
+
+        run_text = '<ul>'
+        for run in runs:
+            run_text += '<li>' + run.type + ', ' + str(run.date) + '</li>'
+        run_text += '</ul>'
+        return run_text
     except Exception as e:
         # e holds description of the error
         error_text = "<p>The error:<br>" + str(e) + "</p>"
         hed = '<h1>Something is broken.</h1>'
         return hed + error_text
-        
-def index():
+
+
     # runs = Run.query.all()
-    return render_template('index.html')
+    # return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
